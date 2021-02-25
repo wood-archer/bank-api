2fe742f93db564382206577bba46330fa399b1df
= <=>
2021-02-25 12:57:55 -0300

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,33 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :bank_api,
+  namespace: BankAPI,
+  ecto_repos: [BankAPI.Repo],
+  generators: [binary_id: true]
+
+# Configures the endpoint
+config :bank_api, BankAPIWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "aRUsq1XWqgZ0KYFyFlAkjkEHDrXpdnD+0f/V/GqVWYfmqG10ugHgvXsU5Z2LWU1q",
+  render_errors: [view: BankAPIWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: BankAPI.PubSub,
+  live_view: [signing_salt: "5ni+MfYN"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
