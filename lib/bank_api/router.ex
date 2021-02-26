87ef9d4bf286d33c05913f0f5939d99c8bfb25c3
= <=>
2021-02-26 17:29:43 -0300

    fix tests

--- lib/bank_api/router.ex
+++ lib/bank_api/router.ex
@@ -1,8 +1,8 @@
 defmodule BankAPI.Router do
-  use Commanded.Commands.Router
+  use Commanded.Commands.Router, application: BankAPI.App
 
   alias BankAPI.Accounts.Aggregates.Account
   alias BankAPI.Accounts.Commands.OpenAccount
 
-  dispatch([OpenAccount], to: Account, function: :open_account, identity: :account_uuid)
+  dispatch([OpenAccount], to: Account, identity: :account_uuid)
 end
